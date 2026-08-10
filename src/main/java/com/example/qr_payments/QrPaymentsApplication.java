de2c95a28077a0b49408f2d22fd9f9ab5b04154e
zacurrya <39706093+Zacurrya@users.noreply.github.com>
2026-08-10 17:31:50 +0100

    initial commit

---
+++ src/main/java/com/example/qr_payments/QrPaymentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.qr_payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrPaymentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrPaymentsApplication.class, args);
+	}
+
+}
